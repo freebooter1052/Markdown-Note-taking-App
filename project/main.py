89ee7a640590de0f2a3232293af453b46079139c
christin <christinjb100@gmail.com>
2026-06-02 19:38:40 +0530

    add FastAPI endpoints for creating and retrieving notes

--- project/main.py
+++ project/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
-from database import engine
+from database import engine,SessionLocal
 from models import Note
+from schemas import Note as NoteCreate
+
 app =FastAPI()
 
 @app.get("/")
@@ -8,4 +10,18 @@ app =FastAPI()
 def home():
     return {"message":"welcome home"}
 
+@app.post("/notes")
+def create_note(note: NoteCreate):
+    db = SessionLocal()
+    new_note = Note(title=note.title, content=note.content)
+    db.add(new_note)
+    db.commit()
+    return {"message": "Note created successfully", "note": new_note}
+
+@app.get("/notes")
+def get_notes():
+    db = SessionLocal()
+    notes = db.query(Note).all()
+    return {"notes": notes}
+    
 Note.metadata.create_all(bind=engine)
